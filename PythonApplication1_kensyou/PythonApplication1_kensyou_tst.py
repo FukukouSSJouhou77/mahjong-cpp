bef0d648d01843bc6e9378635f07643ec2b027bb
umekoujouhouhan <umekoujouhouhan@gmail.com>
2023-01-23 17:53:57 +0900

    2

---
+++ PythonApplication1_kensyou/PythonApplication1_kensyou_tst.py
@@ -0,0 +1,47 @@
+
+import mahjong_pythonlib_mod as pymod
+
+
+
+a=pymod.sample_mod1()
+print("Python Module!")
+handkun=pymod.Hand([int(pymod.Tile.Manzu1)])
+hand1=pymod.Hand([int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu5), int(pymod.Tile.Manzu6), int(pymod.Tile.Manzu7),int(pymod.Tile.Pinzu3), int(pymod.Tile.Pinzu4), int(pymod.Tile.Pinzu5), int(pymod.Tile.Sozu3), int(pymod.Tile.Sozu3), int(pymod.Tile.Sozu6),int(pymod.Tile.Sozu6), int(pymod.Tile.Sozu7)])
+hand2=pymod.Hand([int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu5), int(pymod.Tile.Manzu6), int(pymod.Tile.Manzu7),int(pymod.Tile.Pinzu3), int(pymod.Tile.Pinzu4), int(pymod.Tile.Sozu3), int(pymod.Tile.Sozu3), int(pymod.Tile.Sozu6), int(pymod.Tile.Sozu6),int(pymod.Tile.Sozu7), int(pymod.Tile.Pe)])
+hand3=pymod.Hand([int(pymod.Tile.Manzu1), int(pymod.Tile.Manzu1), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu4), int(pymod.Tile.Manzu5), int(pymod.Tile.Manzu7),
+                int(pymod.Tile.Pinzu9), int(pymod.Tile.Sozu3), int(pymod.Tile.Sozu7), int(pymod.Tile.Sozu9), int(pymod.Tile.Ton), int(pymod.Tile.Pe), int(pymod.Tile.Pe),
+                int(pymod.Tile.Hatu)]);
+hand4=pymod.Hand([int(pymod.Tile.Manzu1), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu3), int(pymod.Tile.Manzu3), int(pymod.Tile.Manzu4), int(pymod.Tile.Manzu9),
+                int(pymod.Tile.Pinzu3), int(pymod.Tile.Pinzu6), int(pymod.Tile.Pinzu8), int(pymod.Tile.Pinzu8), int(pymod.Tile.Sozu1), int(pymod.Tile.Sozu2),
+                int(pymod.Tile.Sozu4), int(pymod.Tile.Sozu5)]);
+hand5=pymod.Hand([int(pymod.Tile.Manzu1), int(pymod.Tile.Manzu2), int(pymod.Tile.Manzu4), int(pymod.Tile.Manzu5), int(pymod.Tile.Manzu5), int(pymod.Tile.Pinzu3),
+                int(pymod.Tile.Pinzu4), int(pymod.Tile.Pinzu5), int(pymod.Tile.Pinzu6), int(pymod.Tile.Pinzu7), int(pymod.Tile.Sozu6), int(pymod.Tile.Sozu7),
+                int(pymod.Tile.Sozu7), int(pymod.Tile.Sozu7)]);
+bakaze=int(pymod.Tile.Ton);
+zikaze=int(pymod.Tile.Ton);
+turn=3;
+syanten_type=1;
+flag = int(pymod.ExpectedValueCalculator.CalcSyantenDown )| int(pymod.ExpectedValueCalculator.CalcTegawari   )| int(pymod.ExpectedValueCalculator.CalcDoubleReach )| int(pymod.ExpectedValueCalculator.CalcIppatu )| int(pymod.ExpectedValueCalculator.CalcHaiteitumo )| int(pymod.ExpectedValueCalculator.CalcUradora )| int(pymod.ExpectedValueCalculator.CalcAkaTileTumo)
+dora_indicators=[int(pymod.Tile.Ton)]
+hand=hand5
+exp_value_calculator=pymod.ExpectedValueCalculator()
+score_calculator=pymod.ScoreCalculator()
+score_calculator.set_bakaze(bakaze)
+score_calculator.set_zikaze(zikaze)
+score_calculator.set_dora_indicators(dora_indicators)
+c,syanten=pymod.SyantenCalculator.calc(hand,syanten_type)
+success, candidates =exp_value_calculator.calc1(hand, score_calculator, dora_indicators, syanten_type, flag)
+if success:
+    None
+else:
+    print("ERR")
+    exit(-1)
+exp_value_calculated = not candidates[0].tenpai_probs
+if exp_value_calculated:
+    pymod.sort_Candicate(candicates,turn)
+print("Tehai: %s, Syanten: %d, Turn: %d" % (hand.to_string(),syanten,turn))
+print(syanten)
+print("test")
+print(pymod.strtest())
+print(a)
+input()
--- src/mahjong/types/tile.hpp
+++ src/mahjong/types/tile.hpp
@@ -69,6 +69,18 @@ struct Tile
         {Sya, "西"},           {Pe, "北"},          {Haku, "白"},
         {Hatu, "發"},          {Tyun, "中"},        {AkaManzu5, "赤五萬"},
         {AkaPinzu5, "赤五筒"}, {AkaSozu5, "赤五索"}};
+
+    static inline const std::map<int, std::string> EnName = {
+        {Null, "Null"},        {Manzu1, "一萬"},    {Manzu2, "二萬"}, {Manzu3, "三萬"},
+        {Manzu4, "四萬"},      {Manzu5, "五萬"},    {Manzu6, "六萬"}, {Manzu7, "七萬"},
+        {Manzu8, "八萬"},      {Manzu9, "九萬"},    {Pinzu1, "一筒"}, {Pinzu2, "二筒"},
+        {Pinzu3, "三筒"},      {Pinzu4, "四筒"},    {Pinzu5, "五筒"}, {Pinzu6, "六筒"},
+        {Pinzu7, "七筒"},      {Pinzu8, "八筒"},    {Pinzu9, "九筒"}, {Sozu1, "一索"},
+        {Sozu2, "二索"},       {Sozu3, "三索"},     {Sozu4, "四索"},  {Sozu5, "五索"},
+        {Sozu6, "六索"},       {Sozu7, "七索"},     {Sozu8, "八索"},  {Sozu9, "九索"},
+        {Ton, "東"},           {Nan, "南"},         {Sya, "西"},      {Pe, "北"},
+        {Haku, "白"},          {Hatu, "發"},        {Tyun, "中"},     {AkaManzu5, "赤五萬"},
+        {AkaPinzu5, "赤五筒"}, {AkaSozu5, "赤五索"}};
 };
 
 /**
